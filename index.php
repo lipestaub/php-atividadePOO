0f885a92b647701abaab840e729582dbd461efb4
Felipe Staub~ <lipestaub@gmail.com>
2024-01-26 08:21:39 -0300

    update

--- index.php
+++ index.php
@@ -5,16 +5,17 @@
     require_once __DIR__ . '/Classes/Pessoa.php';
     require_once __DIR__ . '/Classes/Professor.php';
 
-    $aluno = new Aluno('Felipe', 20, 1234, [10.0, 9.5, 8.0]);
+    $aluno1 = new Aluno('Felipe', 20, 1234, [10.0, 9.5, 8.0]);
+    $aluno2 = new Aluno('Gabriel', 20, 5421, [7.0, 8.5, 9.0]);
+    $aluno3 = new Aluno('Pedro', 20, 2153, [7.0, 9.5, 6.0]);
     $professor = new Professor('Anderson', 30, []);
-    $disciplina = new Disciplina('Estrutura de Dados', 280, $professor, [$aluno]);
+    $disciplina = new Disciplina('Estrutura de Dados', 280, $professor, [$aluno1, $aluno2, $aluno3]);
     $faculdade = new Faculdade('Unisc', [$disciplina]);
-
-    var_dump($faculdade);
 
     $dados = json_encode($faculdade, JSON_PRETTY_PRINT);
 
-    $openedFile = fopen(__DIR__ . '/files/arquivo.json', 'a');
+    $filePath = __DIR__ . '/files/arquivo.json';
+    $openedFile = fopen($filePath, 'a');
     fwrite($openedFile, $dados);
     fclose($openedFile);
 ?>
