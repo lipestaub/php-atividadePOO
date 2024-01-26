0af28535d276273d0c152955cc20642dd3d77e20
Felipe Staub~ <lipestaub@gmail.com>
2024-01-26 11:51:16 -0300

    update

--- Classes/Aluno.php
+++ Classes/Aluno.php
@@ -11,5 +11,21 @@
             $this->matricula = $matricula;
             $this->notas = $notas;
         }
+
+        public function getMatricula() {
+            return $this->matricula;
+        }
+
+        public function setMatricula(int $matricula) {
+            $this->matricula = $matricula;
+        }
+
+        public function getNotas() {
+            return $this->notas;
+        }
+
+        public function setNotas(array $notas) {
+            $this->notas = $notas;
+        }
     }
 ?>
--- index.php
+++ index.php
@@ -8,11 +8,13 @@
     $aluno1 = new Aluno('Felipe', 20, 1234, [10.0, 9.5, 8.0]);
     $aluno2 = new Aluno('Gabriel', 20, 5421, [7.0, 8.5, 9.0]);
     $aluno3 = new Aluno('Pedro', 20, 2153, [7.0, 9.5, 6.0]);
-    $professor = new Professor('Anderson', 30, []);
-    $disciplina = new Disciplina('Estrutura de Dados', 280, $professor, [$aluno1, $aluno2, $aluno3]);
-    $faculdade = new Faculdade('Unisc', [$disciplina]);
+    $professor1 = new Professor('Ricardo', 30, []);
+    $professor2 = new Professor('José', 30, []);
+    $disciplina1 = new Disciplina('Estrutura de Dados', 280, $professor1, [$aluno1, $aluno2, $aluno3]);
+    $disciplina2 = new Disciplina('Matemática', 120, $professor2, [$aluno1, $aluno3]);
+    $faculdade = new Faculdade('Unisc', [$disciplina1, $disciplina2]);
 
-    $dados = json_encode($faculdade, JSON_PRETTY_PRINT);
+    $dados = json_encode($faculdade, JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
 
     $filePath = __DIR__ . '/files/arquivo.json';
     $openedFile = fopen($filePath, 'a');
